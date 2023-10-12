80fb4ccdc9c19e86a2eb4e8505c4d842434f6f9f
Sahiltr <thakursahil1912@gmail.com>
2023-10-12 13:37:13 +0530

    gg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import CustomButton from './Button';
 
-function App() {
+function Counter() {
+  const[count,setCount]=useState(0);
+  const increamentCount=()=>{
+    setCount(count+1);
+  };
+  const decreamentCount=()=>{
+    setCount(count-1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter: {count}</h1>
+      <CustomButton onClick={increamentCount} label="Increament"></CustomButton>
+      <CustomButton onClick={decreamentCount} label="Decreament"></CustomButton>   
     </div>
+  );
+}
+function App(){
+  return(
+    <div className='app'>
+      <Counter/>
+      </div>
   );
 }
 
---
+++ src/Button.js
@@ -0,0 +1,6 @@
+import React from 'react';
+function CustomButton(props){
+    return( <button onClick={props.onClick}>{props.label}</button>);
+}
+
+export default CustomButton;
